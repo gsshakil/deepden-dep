3eb5c6279e41be1f74cdfa99ee9b469c91e1f349
macbook <macbook@macbooks-MacBook-Pro.local>
2017-07-05 00:02:43 +0600

    Added images and footer

--- client/main.js
+++ client/main.js
@@ -20,3 +20,16 @@ Template.hello.events({
     instance.counter.set(instance.counter.get() + 1);
   },
 });
+
+Meteor.startup(() => {
+    // code to run on server at startup
+    smoothScroll.init({
+      selector: '[data-scroll]', // Selector for links (must be a valid CSS selector)
+      selectorHeader: '[data-scroll-header]', // Selector for fixed headers (must be a valid CSS selector)
+      speed: 500, // Integer. How fast to complete the scroll in milliseconds
+      easing: 'easeInOutCubic', // Easing pattern to use
+      updateURL: true, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+      offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+      callback: function ( toggle, anchor ) {} // Function to run after scrolling
+    });
+});
